cd86cf5565e3dcf194e74ee0bb7978038df39872
Jihye19249205 <119666066+Jihye19249205@users.noreply.github.com>
2022-12-27 08:44:37 +0000

    [update]3-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/edit'
-  get '/top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  resources :books do
+    collection do
+      get 'books/:id' => 'books#show'
+      get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+      patch 'books/:id' => 'books#update', as: 'update_book'
+      delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+    end
+  end
 end
